19ec5ee7cc67d49d28b5e7906107950506b82aa9
akshay ashok <akshayashokan1054@gmail.com>
2023-12-11 07:29:11 +0530

    part 3 done

--- app/src/main/java/com/example/unitconverterapp/TopScreen.kt
+++ app/src/main/java/com/example/unitconverterapp/TopScreen.kt
@@ -1,5 +1,6 @@
 package com.example.unitconverterapp
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
@@ -13,6 +14,9 @@ fun TopScreen(list: List<Conversion>) {
         selectedConversion.value=it
     }
     selectedConversion.value?.let {
-        InputBlock(conversion = it, inputText = inputText)
+        InputBlock(conversion = it, inputText = inputText){input->
+            Log.i("","User typed $input")
+
+        }
     }
 }
