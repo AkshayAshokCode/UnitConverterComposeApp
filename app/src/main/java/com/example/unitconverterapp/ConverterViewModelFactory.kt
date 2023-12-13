4213b9aa82860773ee53538d13910a17c354f294
akshay ashok <akshayashokan1054@gmail.com>
2023-12-13 09:03:52 +0530

    DI added

--- app/src/main/java/com/example/unitconverterapp/ConverterViewModelFactory.kt
+++ app/src/main/java/com/example/unitconverterapp/ConverterViewModelFactory.kt
@@ -3,8 +3,9 @@ package com.example.unitconverterapp
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import com.example.unitconverterapp.data.ConverterRepository
+import javax.inject.Inject
 
-class ConverterViewModelFactory(private val converterRepository: ConverterRepository):
+class ConverterViewModelFactory @Inject constructor(private val converterRepository: ConverterRepository):
     ViewModelProvider.NewInstanceFactory() {
     override fun <T : ViewModel> create(modelClass: Class<T>): T =ConverterViewModel(converterRepository) as T
 }
